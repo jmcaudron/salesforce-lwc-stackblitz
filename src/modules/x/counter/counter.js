09a0be149d85733d7a55dc0d75e05319704ba5cf
jmcaudron <jmcaudron@gmail.com>
2023-03-09 01:08:19 +0100

    1 file modified

--- src/modules/x/counter/counter.js
+++ src/modules/x/counter/counter.js
@@ -1,6 +1,9 @@
 import { LightningElement } from 'lwc';
+import pkceChallenge from 'pkce-challenge';
+
 
 export default class extends LightningElement {
+test=pkceChallenge();
     counter = 0;
 
     increment() {
