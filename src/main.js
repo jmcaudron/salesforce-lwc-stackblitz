e590e00ec56959b81bca31842960b432084c642d
Jean-Marc Caudron <jmcaudron@gmail.com>
2023-03-15 10:59:36 +0100

    update with new app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createElement } from "lwc";
 import App from "x/app";
+import Pkce from "pkce/app";
 
 const elm = createElement("x-app", { is: App });
 document.body.appendChild(elm);
+const pkce = createElement("pkce-app", { is: Pkce });
+document.body.appendChild(pkce);
